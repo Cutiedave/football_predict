bf227be59bcda3d0fcd259f1d62032fe91075a62
cutiedadaves <cutiedaves@gmail.com>
2022-03-25 23:01:56 +0100

    'v3'

--- football_prediction/settings.py
+++ football_prediction/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-2)$1@b6xa1i+63es1aix_yga&gk6#7bhs(#$-^b=3s9ky*)n-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['king-predict.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
