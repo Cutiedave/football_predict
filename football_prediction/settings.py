058cda7581c78bb9f474f9547bcf6e3b10f644ee
cutiedadaves <cutiedaves@gmail.com>
2022-03-25 23:55:36 +0100

    'v3'

--- football_prediction/settings.py
+++ football_prediction/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR=os.path.join(BASE_DIR,'/football_prediction')
+#STATICFILES_DIR=os.path.join(BASE_DIR,'/statics')
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
