c8aaa812a1606156bdd9fd8edb8ff1c1d0f800d8
cutiedadaves <cutiedaves@gmail.com>
2022-03-26 10:31:27 +0100

    'v3'

--- football_prediction/settings.py
+++ football_prediction/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2)$1@b6xa1i+63es1aix_yga&gk6#7bhs(#$-^b=3s9ky*)n-q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['king-predict.herokuapp.com','127.0.0.1','localhost']
@@ -132,10 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATICFILES_DIR=os.path.join(BASE_DIR,'/statics')
+#STATICFILES_DIR=os.path.join(BASE_DIR,'travelo-master/')
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
